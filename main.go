c643c9a07c125a73075a9ceeb1d89f04527491fe
Sam <31251023+sbleks@users.noreply.github.com>
2023-11-08 16:49:04 -0600

    feat: linked list cli

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 )
@@ -26,6 +27,65 @@ func addNode(data int) *Node {
 	return newNode
 }
 
+func removeNode(data int) bool {
+	// if you have not added any nodes
+	if head == nil {
+		fmt.Printf("You have no nodes so you can't delete anything\n")
+		return false
+	}
+
+	curr := head
+	prev := head
+
+	for curr != nil {
+		if curr.data == data {
+
+			// if the node you want to remove is the head
+			if curr == head {
+				head = curr.next
+			} else {
+				prev.next = curr.next
+			}
+
+			return true
+
+		}
+
+		prev = curr
+		curr = curr.next
+	}
+
+	return false
+}
+
+func insertNode(data, nodeVal int) bool {
+	var newNode = &Node{data: data}
+
+	if head == nil {
+		fmt.Printf("You have no nodes so you can't delete anything\n")
+		return false
+	}
+
+	curr := head
+	for curr != nil {
+		if curr.data == nodeVal {
+
+			// if you want to insert
+			if curr == head {
+				newNode.next = curr.next
+				head = newNode
+			} else {
+				newNode.next = curr.next
+				curr.next = newNode
+			}
+
+			return true
+		}
+		curr = curr.next
+	}
+	return false
+}
+
 func printNodes() {
 	if head == nil {
 		fmt.Printf("Your list is empty\n")
@@ -33,12 +93,18 @@ func printNodes() {
 	}
 
 	cur := head
-
 	for cur != nil {
-		fmt.Printf("%v->", cur)
+		fmt.Printf("%d->", cur.data)
 		cur = cur.next
 	}
 	fmt.Printf("\n")
+}
+
+func seedNodes() {
+	for i := 0; i < 15; i++ {
+		num := rand.Intn(100)
+		addNode(num)
+	}
 }
 
 func printMenu() {
@@ -47,7 +113,20 @@ func printMenu() {
 	fmt.Printf("\t2. Remove a Node from the list.\n")
 	fmt.Printf("\t3. Insert a Node into the list.\n")
 	fmt.Printf("\t4. Print your list.\n")
-	fmt.Printf("\t5. Quit.\n")
+	fmt.Printf("\t5. Seed list.\n")
+	fmt.Printf("\t6. Quit.\n")
+}
+
+func scanNextNum(scanner *bufio.Scanner) int {
+	scanner.Scan()
+	data := scanner.Text()
+	dataInt, err := strconv.Atoi(data)
+
+	if err != nil {
+		fmt.Printf("The data you passed in isn't a number: %s\n", err)
+	}
+
+	return dataInt
 }
 
 var head *Node
@@ -56,37 +135,50 @@ func main() {
 
 	option := -1
 
-	for option != 5 {
+	for option != 6 {
 		printMenu()
 		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		text := scanner.Text()
-		selectedOption, err := strconv.Atoi(text)
-		if err != nil {
-			fmt.Printf("The option you entered isn't valid, %s\n", err)
-		}
+		selectedOption := scanNextNum(scanner)
 		option = selectedOption
-		if option > 0 && option <= 4 {
+		if option > 0 && option <= 6 {
 			switch option {
 			case 1:
 				// Add node to list
 				fmt.Printf("What value should your node hold?\n")
-				scanner.Scan()
-				data := scanner.Text()
-				dataInt, err := strconv.Atoi(data)
-				if err != nil {
-					fmt.Printf("The data you passed in isn't a number: %s\n", err)
-				}
-				newNode := addNode(dataInt)
+				selectedOption := scanNextNum(scanner)
+				newNode := addNode(selectedOption)
 				fmt.Printf("%v\n", newNode)
+				break
 			case 2:
 				// Remove a node from list
+				fmt.Printf("What node should we remove?\n")
+				printNodes()
+				selectedOption := scanNextNum(scanner)
+				if !removeNode(selectedOption) {
+					fmt.Printf("The node you selected was not found in the list, please try again\n")
+				} else {
+					break
+				}
 			case 3:
 				// Insert a node into the list
+				fmt.Printf("Where should we add a node?\n")
+				printNodes()
+				nodePosition := scanNextNum(scanner)
+				fmt.Printf("What value should your node hold?\n")
+				data := scanNextNum(scanner)
+				if !insertNode(data, nodePosition) {
+					fmt.Printf("The node you selected was not found in the list, please try again\n")
+				} else {
+					break
+				}
 			case 4:
 				// Print list
 				printNodes()
+				break
 			case 5:
+				seedNodes()
+				break
+			case 6:
 				break
 			}
 		}
